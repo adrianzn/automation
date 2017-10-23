e0187856d939a982eccda3bf3b6b1cac5790182f
Adrian Zhang <adrianzn@users.noreply.github.com>
2017-10-23 19:43:18 +0800

    Create calculator_smoke_test.py

---
+++ calculator_smoke_test.py
@@ -0,0 +1,257 @@
+# Author: AdrianZhang
+# Script function: Android计算器的冒烟测试：(整数，小数、负数)的加法、减法、乘法、除法，输入、删除功能
+# Coding Time: 2017/10/23
+
+import time
+from appium import webdriver
+
+def device_info():
+    desired_caps = {}
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '7.1.1'
+    desired_caps['deviceName'] = 'emulator-5554'
+    desired_caps['appPackage'] = 'com.android.calculator2'
+    desired_caps['appActivity'] = '.Calculator'
+    desired_caps['unicodeKeyboard'] = 'True'
+    desired_caps['resetKeyboard'] = 'True'
+    return desired_caps
+
+class CalculatorSmoke(object):
+    def __init__(self):
+        self.desired_caps = device_info()
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_caps)
+
+    def addition_integers(self):
+        # 整数加法："1 + 7"
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_add').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_7').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/eq').click()
+        addition_integers_result = self.driver.find_element_by_id('com.android.calculator2:id/result').text
+        time.sleep(1)
+        self.driver.find_element_by_id('com.android.calculator2:id/clr').click()
+        try:
+            if addition_integers_result == str(addition_integers_correct_value):
+                print("Addition Integers: {} = {} is Pass!".format(addition_integers_demo, addition_integers_result))
+            else:
+                print("Addition Integers calculation is wrong!")
+        except Exception as msg:
+            print(msg)
+
+    def addition_decimals(self):
+        # 小数加法："0.1 + 0.7"
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_0').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/dec_point').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_add').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_0').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/dec_point').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_7').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/eq').click()
+        addition_decimals_result = self.driver.find_element_by_id('com.android.calculator2:id/result').text
+        time.sleep(1)
+        self.driver.find_element_by_id('com.android.calculator2:id/clr').click()
+        try:
+            if addition_decimals_result == str(addition_decimals_correct_value):
+                print("Addition Decimals: {} = {} is Pass!".format(addition_decimals_demo, addition_decimals_result))
+            else:
+                print("Addition Decimals calculation is wrong!")
+        except Exception as msg:
+            print(msg)
+
+    def addition_negative(self):
+        # 负数加法："-1 + (-7)"
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_add').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_7').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/eq').click()
+        addition_negative_result = self.driver.find_element_by_id('com.android.calculator2:id/result').text
+        time.sleep(1)
+        self.driver.find_element_by_id('com.android.calculator2:id/clr').click()
+        try:
+            if addition_negative_result == str(addition_negative_correct_value):
+                print("Addition Negative: {} = {} is Pass!".format(addition_negative_demo, addition_negative_result))
+            else:
+                print("Addition Negative calculation is wrong!")
+        except Exception as msg:
+            print(msg)
+
+    def subtraction_integers(self):
+        # 整数减法："6 - 2"
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_6').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_2').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/eq').click()
+        subtraction_integers_result = self.driver.find_element_by_id('com.android.calculator2:id/result').text
+        time.sleep(1)
+        self.driver.find_element_by_id('com.android.calculator2:id/clr').click()
+        try:
+            if subtraction_integers_result == str(subtraction_integers_correct_value):
+                print("Subtraction Integers: {} = {} is Pass!".format(subtraction_integers_demo, subtraction_integers_result))
+            else:
+                print("Subtraction Integers calculation is wrong!")
+        except Exception as msg:
+            print(msg)
+
+    def subtraction_decimals(self):
+        # 小数减法："0.6 - 0.2"
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_0').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/dec_point').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_6').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_0').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/dec_point').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_2').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/eq').click()
+        subtraction_decimals_result = self.driver.find_element_by_id('com.android.calculator2:id/result').text
+        time.sleep(1)
+        self.driver.find_element_by_id('com.android.calculator2:id/clr').click()
+        try:
+            if subtraction_decimals_result == str(subtraction_decimals_correct_value):
+                print("Subtraction Decimals: {} = {} is Pass!".format(subtraction_decimals_demo, subtraction_decimals_result))
+            else:
+                print("Subtraction Decimals calculation is wrong!")
+        except Exception as msg:
+            print(msg)
+
+    def subtraction_negative(self):
+        # 负数减法："-6 - (-2)"
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_6').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.swipe(1060, 1300, 300, 1300, 1000)
+        self.driver.find_element_by_id('com.android.calculator2:id/lparen').click()
+        self.driver.swipe(300, 1300, 1060, 1300, 1000)
+        self.driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+        self.driver.find_element_by_id('com.android.calculator2:id/digit_2').click()
+        self.driver.swipe(1060, 1300, 300, 1300, 1000)
+        self.driver.find_element_by_id('com.android.calculator2:id/rparen').click()
+        self.driver.swipe(300, 1300, 1060, 1300, 1000)
+        self.driver.find_element_by_id('com.android.calculator2:id/eq').click()
+        subtraction_negative_result = self.driver.find_element_by_id('com.android.calculator2:id/result').text
+        time.sleep(1)
+        self.driver.find_element_by_id('com.android.calculator2:id/clr').click()
+        try:
+            if subtraction_negative_result == str(subtraction_negative_correct_value):
+                print("Subtraction Negative: {} = {} is Pass!".format(subtraction_negative_demo, subtraction_negative_result))
+            else:
+                print("Subtraction Negative calculation is wrong!")
+        except Exception as msg:
+            print(msg)
+
+    def multiplication_integers(self):
+        # 整数乘法："5 * 4"
+        pass
+
+    def multiplication_decimals(self):
+        # 小数乘法："0.5 * 0.4"
+        pass
+
+    def multiplication_negative(self):
+        # 负数乘法："-5 * 4"
+        pass
+
+
+    def division_integers(self):
+        # 整数除法："9 / 3"
+        pass
+
+    def division_decimals(self):
+        # 小数除法："0.9 / 0.3"
+        pass
+
+    def division_negative(self):
+        # 负数除法："-9 / 3"
+        pass
+
+
+    def input_button(self):
+        pass
+
+    def delete_button(self):
+        pass
+
+    def quit(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    calculator = CalculatorSmoke()
+    # 整数加法： addition_integers
+    addition_integers_demo = "1 + 7"
+    addition_integers_correct_value = 8
+    addition_integers = calculator.addition_integers()
+    # 小数加法： addition_decimals
+    addition_decimals_demo = "0.1 + 0.7"
+    addition_decimals_correct_value = 0.8
+    addition_decimals = calculator.addition_decimals()
+    # 负数加法： addition_negative
+    addition_negative_demo = "-1 + (-7)"
+    addition_negative_correct_value = -8
+    addition_negative = calculator.addition_negative()
+
+    # 整数减法：subtraction_integers
+    subtraction_integers_demo = "6 - 2"
+    subtraction_integers_correct_value = 4
+    subtraction_integers = calculator.subtraction_integers()
+    # 小数减法：subtraction_decimals
+    subtraction_decimals_demo = "0.6 - 0.2"
+    subtraction_decimals_correct_value = 0.4
+    subtraction_decimals = calculator.subtraction_decimals()
+    # 负数减法：subtraction_negative
+    subtraction_negative_demo = "-6 - (-2)"
+    subtraction_negative_correct_value = -4
+    subtraction_negative = calculator.subtraction_negative()
+
+    # 整数乘法：multiplication_integers
+    multiplication_integers_demo = "5 * 4"
+    multiplication_integers_correct_value = 20
+    multiplication_integers = calculator.multiplication_integers()
+    # 小数乘法：multiplication_decimals
+    multiplication_decimals_demo = "0.5 * 0.4"
+    multiplication_decimals_correct_value = 0.2
+    multiplication_decimals = calculator.multiplication_decimals()
+    # 负数乘法：multiplication_negative
+    multiplication_negative_demo = "-5 * 4"
+    multiplication_negative_correct_value = -20
+    multiplication_negative = calculator.multiplication_negative()
+
+    # 整数除法：division_integers
+    division_integers_demo = "9 / 3"
+    division_integers_correct_value = 3
+    division_integers = calculator.division_integers()
+    # 小数除法：division_decimals
+    division_decimals_demo = "0.9 / 0.3"
+    division_decimals_correct_value = 0.3
+    division_decimals = calculator.division_decimals()
+    # 负数除法：division_negative
+    division_negative_demo = "-9 / 3"
+    division_negative_correct_value = -3
+    division_negative = calculator.division_negative()
+
+    # input 0,1,2,3,4,5,6,7,8,9
+    input_button_0 = "0"
+    input_button_1 = "1"
+    input_button_2 = "2"
+    input_button_3 = "3"
+    input_button_4 = "4"
+    input_button_5 = "5"
+    input_button_6 = "6"
+    input_button_7 = "7"
+    input_button_8 = "8"
+    input_button_9 = "9"
+    input_button_0_correct_value = 0
+    input_button_1_correct_value = 1
+    input_button_2_correct_value = 2
+    input_button_3_correct_value = 3
+    input_button_4_correct_value = 4
+    input_button_5_correct_value = 5
+    input_button_6_correct_value = 6
+    input_button_7_correct_value = 7
+    input_button_8_correct_value = 8
+    input_button_9_correct_value = 9
+    input_button = calculator.input_button()
+
+    # delete 0,1,2,3,4,5,6,7,8,9
+    delete_button = calculator.delete_button()
