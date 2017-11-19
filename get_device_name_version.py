c23003e2425af3fa4507e56a0308a0fe32d442fb
Adrian Zhang <adrianzn@163.com>
2017-11-19 19:07:36 +0800

    Create get_device_name_version.py

---
+++ get_device_name_version.py
@@ -0,0 +1,27 @@
+import os
+
+def get_device_name():
+    try:
+        content = os.popen("adb devices").readlines()
+        for each in content:
+            if "\t" in each:
+                device_name = each.split("\t")[0]
+                return device_name
+    except Exception as error:
+        print(error)
+
+def get_platform_version():
+    try:
+        content = os.popen("adb shell cat /system/build.prop").readlines()
+        for each in content:
+            if "version.release" in each:
+                device_version = each.split("=")[-1].replace("\n", "")
+                return device_version
+    except Exception as error:
+        print(error)
+
+if __name__ == '__main__':
+    device_name = get_device_name()
+    device_version = get_platform_version()
+    print("The device name is '{}', and device version is '{}'".format(device_name, device_version))
+
