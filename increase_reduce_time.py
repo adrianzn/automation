2ea8ef69333a0015bc3cd72745575c5135b63ff2
Adrian Zhang <adrianzn@163.com>
2018-02-06 20:43:50 +0800

    Update increase_reduce_time.py

--- increase_reduce_time.py
+++ increase_reduce_time.py
@@ -1,20 +1,31 @@
 # Author: AdrianZhang
 # Coding Time: 2018/02/06
-# Script Function: Increase and reduce the time.
+# Script Function: Through this script, you can get before time, current time and after time.
 
 import time
 from datetime import datetime
 
-def time_count(times):
-    current_time = time.mktime(datetime.now().timetuple())
+def before_time():
+    history_time = time.mktime(datetime.now().timetuple()) - times
+    before_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(history_time))
+    return history_time, before_time
+
+def current_time():
+    now_time = time.mktime(datetime.now().timetuple())
+    current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(now_time))
+    return now_time, current_time
+
+def after_time():
     future_time = time.mktime(datetime.now().timetuple()) + times
-    start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(current_time))
-    end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(future_time))
-    return current_time, future_time, start_time, end_time
+    after_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(future_time))
+    return future_time, after_time
 
 if __name__ == '__main__':
     times = 60 * 3  # total: 3 minutes
-    time_result = time_count(times)
-    print('Start Time: {} >>> {}'
-          '\nEnd   Time: {} >>> {}'.format(time_result[0], time_result[2], time_result[1], time_result[3]))
+    before_time = before_time()
+    current_time = current_time()
+    after_time = after_time()
+    print("Before Time: \n{}\n{}\n".format(before_time[0], before_time[1]))
+    print("Current Time: \n{}\n{}\n".format(current_time[0], current_time[1]))
+    print("After Time: \n{}\n{}\n".format(after_time[0], after_time[1]))
 
