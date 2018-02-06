eab9302bc471ea010a681a9fe3ceae6617e2467f
Adrian Zhang <adrianzn@163.com>
2018-02-06 15:18:52 +0800

    Update web_md5_verify.py

--- web_md5_verify.py
+++ web_md5_verify.py
@@ -1,12 +1,12 @@
 # Author: AdrianZhang
 # Coding Time: 2017/11/28
-# Script Function: 校验一个网页是否更新。
+# Script Function: Check the web page is updated or not updated.
 
 import requests
 import hashlib
 import os
 
-url = "http://news.baidu.com"
+url = "http://your.web.address"
 html = requests.get(url).text.encode('utf-8-sig')
 new_html_md5 = hashlib.md5(html).hexdigest()
 
@@ -17,13 +17,13 @@ if os.path.exists('old_html_md5.txt'):
     if new_html_md5 != old_md5:
         with open('old_html_md5.txt', 'w') as f:
             old_md5 = f.write(new_html_md5)
-        print('网页数据已更新！')
+        print('HaHa, The Web is update!')
 
     else:
-        print('网页数据未更新！')
+        print('Sorry, The Web is not update!')
 
 else:
     with open('old_html_md5.txt', 'w') as f:
         f.write(new_html_md5)
-    print('哈哈，这是第一次生成md5文件啊~')
+    print('Oh, this is the first time to make md5 file.')
 
